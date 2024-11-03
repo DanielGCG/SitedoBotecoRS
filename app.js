87035caefc9cb557c6995f391e7575e95b67f444
DanielGCG <danielgcg@dcc.ufrj.br>
2024-11-03 16:10:22 -0300

    tentativa de resolver o app.js

--- app.js
+++ app.js
@@ -4,8 +4,7 @@ const express = require('express');
 const expressLayout = require('express-ejs-layouts');
 
 const app = express();
-const PORT = 5000 || process.env.PORT;
-
+const PORT = process.env.PORT || 5000;
 
 app.use(express.static('public'));
 
@@ -14,9 +13,8 @@ app.use(expressLayout);
 app.set('layout', './layouts/main');
 app.set('view engine', 'ejs');
 
-
 app.use('/', require('./server/routes/main'));
 
-app.listen(PORT, () =>{
-    console.log('App listening on port 5000 or defined by host');
-})
+app.listen(PORT, () => {
+    console.log(`App listening on port ${PORT}`);
+});
