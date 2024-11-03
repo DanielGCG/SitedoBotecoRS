373babc42911917ee943776a6b778307d7e73ccd
DanielGCG <danielgcg@dcc.ufrj.br>
2024-11-03 19:10:02 -0300

    Página 404 adicionada!
    Com a gabi!!

--- server/routes/main.js
+++ server/routes/main.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-// Routes
+/* ----- Routes ----- */
 router.get('', (req, res) =>{
     const locals = {
         title: "Site do Boteco do R$",
@@ -12,12 +12,32 @@ router.get('', (req, res) =>{
 });
 
 router.get('/lista', (req, res) =>{
+    const locals = {
+        title: "Lista de Filmes/Séries",
+        description: "Oficial hein! :D"
+    }
+
     res.render('lista');
 });
 
 
 router.get('/about', (req, res) =>{
+    const locals = {
+        title: "Sobre",
+        description: "Oficial hein! :D"
+    }
+    
     res.render('about');
+});
+
+// Rota para página não encontrada
+router.use((req, res) => {
+    const locals = {
+        title: "Página não encontrada",
+        description: "Se ferrou KKKKKK"
+    }
+    res.status(404);
+    res.render('layouts/404', { layout: false });
 });
 
 module.exports = router;
