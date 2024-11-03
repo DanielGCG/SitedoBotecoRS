53fa2a74e8b6ce96b11785e9820c6738ca30d162
DanielGCG <danielgcg@dcc.ufrj.br>
2024-11-03 17:55:08 -0300

    a

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const express = require('express');
 const expressLayout = require('express-ejs-layouts');
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(express.static('public'));
 
