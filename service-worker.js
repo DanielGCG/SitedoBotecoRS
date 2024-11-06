3120d4be85b7f7adf12d4f397a8c6bb653a357df
DanielGCG <danielgcg@dcc.ufrj.br>
2024-11-05 21:40:10 -0300

    Tentativa 2

---
+++ service-worker.js
@@ -0,0 +1,32 @@
+const CACHE_NAME = 'v1';
+const CACHE_ASSETS = [
+    '/',
+    '/index.ejs',               // Página principal (renderizada via EJS)
+    '/404.ejs',                  // Página 404
+    '/css/index.css',           // Estilos principais
+    '/css/style.css',           // Estilos adicionais
+    '/js/script.js',            // Scripts JS principais
+    '/img/gabi404.png',         // Imagem de erro 404
+    '/img/icon-192x192.png',    // Ícone de 192x192
+    '/img/icon-512x512.png',    // Ícone de 512x512
+    '/img/papeldeparede.jpg',   // Imagem de fundo
+    '/img/plaquinha.png',       // Imagem da plaquinha
+    '/img/imagemdodia.jpg'      // Imagem do dia
+];
+
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then(cache => {
+            console.log('Cache assets');
+            return cache.addAll(CACHE_ASSETS);
+        })
+    );
+});
+
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request).then(cacheResponse => {
+            return cacheResponse || fetch(event.request);
+        })
+    );
+});
