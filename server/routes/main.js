1968eade39f67e39392553bab6c559f88a6e0e95
DanielGCG <danielgcg@dcc.ufrj.br>
2024-11-05 22:38:59 -0300

    update

--- server/routes/main.js
+++ server/routes/main.js
@@ -2,40 +2,36 @@ const express = require('express');
 const router = express.Router();
 
 /* ----- Routes ----- */
-router.get('', (req, res) =>{
+router.get('', (req, res) => {
     const locals = {
         title: "Site do Boteco do R$",
         description: "Oficial hein! :D"
     }
-
-    res.render('index', { locals });
+    res.render('index', { locals: locals });
 });
 
-router.get('/lista', (req, res) =>{
+router.get('/lista', (req, res) => {
     const locals = {
         title: "Lista de Filmes/Séries",
         description: "Oficial hein! :D"
     }
-
-    res.render('lista');
+    res.render('lista', { locals: locals });
 });
 
-router.get('/galeria', (req, res) =>{
+router.get('/galeria', (req, res) => {
     const locals = {
         title: "Galeria",
         description: "Muita arte aqui!"
     }
-
-    res.render('galeria');
+    res.render('galeria', { locals: locals });
 });
 
-router.get('/about', (req, res) =>{
+router.get('/about', (req, res) => {
     const locals = {
         title: "Sobre",
         description: "Oficial hein! :D"
     }
-    
-    res.render('about');
+    res.render('about', { locals: locals });
 });
 
 // Rota para página não encontrada
@@ -45,7 +41,7 @@ router.use((req, res) => {
         description: "Se ferrou KKKKKK"
     }
     res.status(404);
-    res.render('layouts/404', { layout: false });
+    res.render('layouts/404', { layout: false, locals: locals });
 });
 
 module.exports = router;
