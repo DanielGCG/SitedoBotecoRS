b1a4b1adb58fafd33a083da54d0d96b377b55fe5
DanielGCG <danielgcg@dcc.ufrj.br>
2024-11-09 00:47:52 -0300

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ require('dotenv').config();
 const express = require('express');
 const expressLayout = require('express-ejs-layouts');
 const cors = require('cors');
-const { TwitterApi } = require('twitter-api-v2');  // Importar a biblioteca do Twitter
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -21,32 +20,6 @@ app.set('view engine', 'ejs');
 
 // Serve arquivos estáticos
 app.use(express.static('public'));
-
-// Configuração do Twitter API
-const twitterClient = new TwitterApi({
-  appKey: process.env.TWITTER_API_KEY,         
-  appSecret: process.env.TWITTER_API_SECRET,   
-  accessToken: process.env.TWITTER_ACCESS_TOKEN,  
-  accessSecret: process.env.TWITTER_ACCESS_SECRET,  
-});
-
-const rwClient = twitterClient.readWrite;
-
-// Rota para postar no Twitter
-app.post('/tweet', async (req, res) => {
-  const { text } = req.body;  // Recebe o texto do tweet enviado na requisição
-
-  if (!text) {
-    return res.status(400).send('Texto do tweet é obrigatório.');
-  }
-
-  try {
-    const tweet = await rwClient.v2.tweet(text);  // Envia o tweet com o texto
-    res.send('Tweet enviado com sucesso!');
-  } catch (error) {
-    res.status(500).send('Erro ao postar o tweet: ' + error.message);
-  }
-});
 
 // Rota principal
 app.use('/', require('./server/routes/main'));
