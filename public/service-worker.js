0054bdde2c3b5b24b4b7ffeacb7f187f125db491
DanielGCG <danielgcg@dcc.ufrj.br>
2024-11-08 22:04:09 -0300

    Arrumei os service-workers

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,29 +1,8 @@
-importScripts('https://www.gstatic.com/firebasejs/9.6.1/firebase-app.js');
-importScripts('https://www.gstatic.com/firebasejs/9.6.1/firebase-messaging.js');
-require('dotenv').config();
-
-// Configuração do Firebase
-const firebaseConfig = {
-    apiKey: process.env.apiKey.apiKey,
-    authDomain: process.env.authDomain,
-    projectId: process.env.projectId,
-    storageBucket: process.env.storageBucket,
-    messagingSenderId: process.env.messagingSenderId,
-    appId: process.env.appId
-};
-
-// Inicializa o Firebase
-firebase.initializeApp(firebaseConfig);
-
-// Inicializa o Firebase Messaging
-const messaging = firebase.messaging();
-
-// Cache e assets
 const CACHE_NAME = 'v1';
 const CACHE_ASSETS = [
     '/',
     '/index.ejs',               // Página principal (renderizada via EJS)
-    '/404.ejs',                 // Página 404
+    '/404.ejs',                  // Página 404
     '/css/index.css',           // Estilos principais
     '/css/style.css',           // Estilos adicionais
     '/js/script.js',            // Scripts JS principais
@@ -35,7 +14,6 @@ const CACHE_ASSETS = [
     '/service-worker.js'
 ];
 
-// Evento de instalação do service worker
 self.addEventListener('install', (event) => {
     event.waitUntil(
         caches.open(CACHE_NAME).then(cache => {
@@ -45,37 +23,15 @@ self.addEventListener('install', (event) => {
     );
 });
 
-// Evento de fetch para fornecer resposta do cache ou da rede
 self.addEventListener('fetch', (event) => {
     event.respondWith(
         caches.match(event.request).then(cacheResponse => {
+            // Retorna do cache se disponível ou tenta buscar na rede
             return cacheResponse || fetch(event.request)
                 .catch(() => {
+                    // Caso a rede falhe, podemos fornecer uma resposta de fallback
                     return caches.match('/404.ejs');
                 });
         })
-    );
-});
-
-// Evento de push para exibir notificações
-self.addEventListener('push', (event) => {
-    const notificationOptions = {
-        body: event.data ? event.data.text() : 'Você tem uma nova notificação!',
-        icon: '/img/icon-192x192.png',
-        badge: '/img/icon-192x192.png',
-    };
-
-    event.waitUntil(
-        self.registration.showNotification('Nova Notificação!', notificationOptions)
-    );
-});
-
-// Evento de clique na notificação
-self.addEventListener('notificationclick', (event) => {
-    event.notification.close(); // Fecha a notificação ao clicar
-
-    // Pode-se adicionar ações, como redirecionar o usuário ao clicar na notificação
-    event.waitUntil(
-        clients.openWindow('/') // Abre a página principal
     );
 });
