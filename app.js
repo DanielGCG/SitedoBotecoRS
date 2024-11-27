5b208236ad1c273c860782ac4f375ea137af8613
DanielGCG <danielgcg@dcc.ufrj.br>
2024-11-27 19:26:23 -0300

    update no app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 const express = require('express');
 const expressLayout = require('express-ejs-layouts');
 const cors = require('cors');
-const { TwitterApi } = require('twitter-api-v2');  // Importar a biblioteca do Twitter
+const { TwitterApi } = require('twitter-api-v2'); // Importar a biblioteca do Twitter
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -13,6 +13,14 @@ app.use(cors());
 
 // Middleware para entender o corpo da requisição como JSON
 app.use(express.json());
+
+// Middleware para redirecionar para o domínio correto
+app.use((req, res, next) => {
+  if (req.headers.host === 'boteco.live') {
+    return res.redirect(301, `https://www.boteco.live${req.url}`);
+  }
+  next();
+});
 
 // Templating Engine
 app.use(expressLayout);
@@ -24,24 +32,24 @@ app.use(express.static('public'));
 
 // Configuração do Twitter API
 const twitterClient = new TwitterApi({
-  appKey: "157Atzi8sPb2kdKuRP92QzmU7",         
-  appSecret: "2nvykVjG9hmgeuVgoGnQORf7e9OgXMTiKTc2vW66o0iY5mcQdD",   
-  accessToken: "1816898320909934592-PJRI2dgehtUcXIlucAcGnham2xmdfd",  
-  accessSecret: "0TkHDoWVPiWpnhCqzpcx3Rz47mGzD6xTHlwlR49QTX5ji",  
+  appKey: "157Atzi8sPb2kdKuRP92QzmU7",
+  appSecret: "2nvykVjG9hmgeuVgoGnQORf7e9OgXMTiKTc2vW66o0iY5mcQdD",
+  accessToken: "1816898320909934592-PJRI2dgehtUcXIlucAcGnham2xmdfd",
+  accessSecret: "0TkHDoWVPiWpnhCqzpcx3Rz47mGzD6xTHlwlR49QTX5ji",
 });
 
 const rwClient = twitterClient.readWrite;
 
 // Rota para postar no Twitter
 app.post('/tweet', async (req, res) => {
-  const { text } = req.body;  // Recebe o texto do tweet enviado na requisição
+  const { text } = req.body; // Recebe o texto do tweet enviado na requisição
 
   if (!text) {
     return res.status(400).send('Texto do tweet é obrigatório.');
   }
 
   try {
-    const tweet = await rwClient.v2.tweet(text);  // Envia o tweet com o texto
+    const tweet = await rwClient.v2.tweet(text); // Envia o tweet com o texto
     res.send('Tweet enviado com sucesso!');
   } catch (error) {
     res.status(500).send('Erro ao postar o tweet: ' + error.message);
