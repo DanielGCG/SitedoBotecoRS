ef9bc75060224ae0a2fbe71e52801d5cd3a6a55e
DanielGCG <danielgcg@dcc.ufrj.br>
2025-01-08 00:49:34 -0300

    Cutucar cooldown
    45 segundos entre uma cutucada e outra

---
+++ public/pages/cutucar/autentication.js
@@ -0,0 +1,82 @@
+// Verificar se a senha inserida está correta
+async function checkPassword() {
+    const senha = document.getElementById("password").value.trim(); // Obter senha do input
+
+    try {
+        const response = await fetch('/verify-senha-cutucar', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ senha }), // Enviar senha como JSON
+        });
+
+        const data = await response.json(); // Converter a resposta para JSON
+
+        if (data.success) {
+            // Marca o usuário como validado
+            localStorage.setItem("validacaoUsuario", "true");
+            console.log("Validação do Usuário:", localStorage.getItem("validacaoUsuario"));
+            document.getElementById("popup-senha").style.display = "none"; // Esconde o popup
+        } else {
+            alert("Senha incorreta. Tente novamente.");
+        }
+    } catch (error) {
+        console.error("Erro ao verificar senha:", error);
+        alert("Erro ao verificar a senha. Tente novamente mais tarde.");
+    }
+}
+
+function checkUserAutentication() {
+    const validacaoUsuario = (localStorage.getItem("validacaoUsuario") === "true" || localStorage.getItem("validacaoUsuario") === true);
+    console.log("Validação do Usuário:", validacaoUsuario);
+    if (!validacaoUsuario) {
+        document.getElementById("popup-senha").style.display = ""; // Mostra o popup
+    }
+}
+
+function setLastCutucadaTime() {
+    // Obtém a data e hora atuais
+    const currentDate = new Date();
+
+    // Armazena no localStorage no formato ISO
+    localStorage.setItem("LastCutucadaTime", currentDate.toISOString());
+}
+
+function checkUserLastCutucadaTime() {
+    // Obtém o valor de LastCutucadaTime do localStorage
+    const LastCutucadaTime = localStorage.getItem("LastCutucadaTime");
+
+    // Verifica se o valor de LastCutucadaTime é válido
+    if (LastCutucadaTime === null) {
+        // Se LastCutucadaTime for null, considera que a pessoa nunca cutucou antes
+        console.log("Nunca cutucou antes. Validação: true");
+        return true; // Valida a pessoa
+    } else {
+        // Se LastCutucadaTime existe, calcula a diferença de tempo em segundos
+        const currentTime = new Date();
+        const lastCutucadaDate = new Date(LastCutucadaTime);
+        const timeDifferenceInSeconds = (currentTime - lastCutucadaDate) / 1000;
+
+        console.log("Diferença de tempo em segundos:", timeDifferenceInSeconds);
+
+        // Verifica se já passaram pelo menos 45 segundos
+        if (timeDifferenceInSeconds >= 45) {
+            console.log("Mais de 45 segundos passaram. Validação: true");
+            return true; // Valida a pessoa
+        } else {
+            console.log("Menos de 45 segundos. Validação: false");
+            return false; // Não valida a pessoa
+        }
+    }
+}
+
+// Verificar se o usuário já está autenticado
+document.addEventListener("DOMContentLoaded", () => {
+    if (document.getElementById("popup-senha")) {
+        checkUserAutentication();
+    }
+    clearMidiaInput();
+    clearMentionInput();
+    clearMessageTextInput();
+});
---
+++ public/pages/cutucar/constants.js
@@ -0,0 +1,39 @@
+/* ----- CONSTANTES E VARIÁVEIS ----- */
+const suggestionsList = [
+    '@botdoboteco',
+    '@Bagriieeel',
+    '@carollssssssss',
+    '@chammittoo',
+    '@Darksols_',
+    '@elonmusk',
+    '@f3rnandesz',
+    '@GabrielXoxota',
+    '@Gb_santos1811',
+    '@gcg_daniel',
+    '@giapropria',
+    '@girlon_tv',
+    '@i2camacho',
+    '@jairbolsonaro',
+    '@JYPETWICE',
+    '@jogadorStick',
+    '@lbbhangelita',
+    '@Liraotrem',
+    '@LulaOficial',
+    '@oleonamjoao',
+    '@palettelilith',
+    '@Regis_trado18',
+    '@snake_sorrel',
+    '@Tal_Lento',
+    '@taylorswift13',
+    '@whoisnenes',
+    '@yuzzu_94'
+];
+
+const apiKey = 'EM9bJD4ixcjo1H4gqPgH6vJ4g3cp1PK6';
+const giphyUrl = 'https://api.giphy.com/v1/gifs/search';
+const selectGifButton = document.getElementById('selectGifButton');
+const mediaFileInput = document.getElementById('mediaFile');
+const popupGif = document.getElementById('popup-gif');
+const popupGifContainer = document.getElementById('popup-gif-gifs');
+const mentionInput = document.getElementById('mention');
+const suggestionsDiv = document.getElementById('suggestions');
